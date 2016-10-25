1d6fc248495234c0b829875efe38b044ae629485
Fabien Agranier <fabien@agranier.com>
2016-10-25 16:59:06 +0200

    missing use statement

--- src/Model/ModelAbstract.php
+++ src/Model/ModelAbstract.php
@@ -2,13 +2,14 @@
 
 namespace Marketplex\Api\Model;
 
+use DateTime;
 use JsonSerializable;
 
 abstract class ModelAbstract implements JsonSerializable {
     public function jsonSerialize() {
         $data = [];
         foreach($this as $key=>$val) {
-            if($val instanceof \DateTime) {
+            if($val instanceof DateTime) {
                 $data[$key] = $val->format(DateTime::ISO8601);
             } else {
                 $data[$key] = $val;
