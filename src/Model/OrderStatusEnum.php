0120d3059bd07ecbe8884307bfc4e4144e70d463
Fabien Agranier <fabien@agranier.com>
2016-11-08 16:08:20 +0100

    TYPO FIX

--- src/Model/OrderStatusEnum.php
+++ src/Model/OrderStatusEnum.php
@@ -2,7 +2,7 @@
 namespace Marketplex\Api\Model;
 
 class OrderStatusEnum extends EnumAbstract {
-    const UNSHIPPED         = "UNISHIPPED";
+    const UNSHIPPED         = "UNSHIPPED";
     const SHIPPED           = "SHIPPED";
     const PARTIALLY_SHIPPED = "PARTIALLY_SHIPPED";
     const PENDING           = "PENDING";
