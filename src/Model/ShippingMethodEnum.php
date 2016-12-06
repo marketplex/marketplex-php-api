b5bae20a4bff0d627c771f41e98d9ddab0a6abeb
Fabien Agranier <fabien@agranier.com>
2016-12-06 16:21:50 +0100

    Add Amazon AFN fulfillment shipping methods

--- src/Model/ShippingMethodEnum.php
+++ src/Model/ShippingMethodEnum.php
@@ -4,4 +4,8 @@ namespace Marketplex\Api\Model;
 class ShippingMethodEnum extends EnumAbstract {
     const STANDARD = "Standard";
     const EXPRESS = "Express";
+    
+    // Amazon Fulfillment Network (FBA - Fulfilled By Amazon)
+    const AFN_STANDARD = "AFN Standard";
+    const AFN_EXPRESS = "AFN Express";
 }
