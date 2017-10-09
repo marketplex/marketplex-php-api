9d06d3c02b4f42e0ca8d37f75037534fe1db8252
Fabien Agranier <fabien@agranier.com>
2017-10-09 18:09:32 +0200

    adds shipping methods to match Amazon ServiceLevel NextDay,SameDay,SecondDay

--- src/Model/ShippingMethodEnum.php
+++ src/Model/ShippingMethodEnum.php
@@ -4,8 +4,13 @@ namespace Marketplex\Api\Model;
 class ShippingMethodEnum extends EnumAbstract {
     const STANDARD = "Standard";
     const EXPRESS = "Express";
-    
+        
     // Amazon Fulfillment Network (FBA - Fulfilled By Amazon)
     const AFN_STANDARD = "AFN Standard";
     const AFN_EXPRESS = "AFN Express";
+    
+    // Amazon Merchant Fullfillment (FBM - Fullfilled By Merchant)
+    const SECOND_DAY        = "SecondDay";
+    const SAME_DAY          = "SameDay";
+    const NEXT_DAY          = "NextDay";
 }
