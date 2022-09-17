83abfd6969211c91765d6ea16c4b13b02953a0de
Fabien Agranier <fabien@agranier.com>
2022-09-17 15:44:21 +0200

    Adds PENDING_ACCEPTATION order status

--- src/Model/OrderStatusEnum.php
+++ src/Model/OrderStatusEnum.php
@@ -2,9 +2,10 @@
 namespace Marketplex\Api\Model;
 
 class OrderStatusEnum extends EnumAbstract {
-    const UNSHIPPED         = "UNSHIPPED";
-    const SHIPPED           = "SHIPPED";
-    const PARTIALLY_SHIPPED = "PARTIALLY_SHIPPED";
-    const PENDING           = "PENDING";
-    const CANCELLED         = "CANCELLED";
+    const UNSHIPPED           = "UNSHIPPED";
+    const SHIPPED             = "SHIPPED";
+    const PARTIALLY_SHIPPED   = "PARTIALLY_SHIPPED";
+    const PENDING             = "PENDING";
+    const PENDING_ACCEPTATION = "PENDING_ACCEPTATION";
+    const CANCELLED           = "CANCELLED";
 }
